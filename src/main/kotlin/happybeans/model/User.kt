b9d1a6ef1880756eee2f1a0712924e72bbae0d31
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-13 17:08:00 +0200

    refactor(User-Entity): add first-name, createdAt and updatedAt

--- src/main/kotlin/happybeans/model/User.kt
+++ src/main/kotlin/happybeans/model/User.kt
@@ -9,6 +9,9 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "users")
@@ -17,15 +20,23 @@ class User(
     var email: String,
     @Column(name = "password", nullable = false)
     var password: String,
-    @Column(name = "name", nullable = false)
-    var name: String,
+    @Column(name = "first_name", nullable = false)
+    var firstName: String,
+    @Column(name = "last_name", nullable = false)
+    var lastName: String,
     @Column(name = "role", nullable = false)
     @Enumerated(EnumType.STRING)
     var role: UserRole = UserRole.USER,
+    @CreationTimestamp
+    var createdAt: LocalDateTime? = null,
+    @UpdateTimestamp
+    var updatedAt: LocalDateTime? = null,
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
     val restaurants: List<String> = listOf(),
     val dislikes: List<String> = listOf(),
     val preferences: List<String> = listOf(),
-)
+) {
+    fun fullName(): String = "$firstName $lastName"
+}
