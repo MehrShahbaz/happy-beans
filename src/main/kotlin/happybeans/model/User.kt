4ef43e5e51835517618d94cb850817d539deabfd
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-13 14:09:08 +0200

    refactor: lint

--- src/main/kotlin/happybeans/model/User.kt
+++ src/main/kotlin/happybeans/model/User.kt
@@ -25,7 +25,6 @@ class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
-
     val restaurants: List<String> = listOf(),
     val dislikes: List<String> = listOf(),
     val preferences: List<String> = listOf(),
