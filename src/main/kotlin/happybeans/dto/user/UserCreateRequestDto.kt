2f8d1f0d4b7675d54df1e016c3985d99449c52d4
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-13 17:10:32 +0200

    feat: Valid user request dto

---
+++ src/main/kotlin/happybeans/dto/user/UserCreateRequestDto.kt
@@ -0,0 +1,16 @@
+package happybeans.dto.user
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
+
+class UserCreateRequestDto(
+    @field:NotBlank(message = "Emailcannot be blank")
+    @field:Email(message = "Must be a valid email address")
+    var email: String,
+    @field:Length(min = 6, message = "Password must be at least 6 characters")
+    var password: String,
+    @field:Length(min = 3, message = "FirstName must be at least 3 characters")
+    var firstName: String,
+    var lastName: String,
+)
