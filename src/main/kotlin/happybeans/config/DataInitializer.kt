ba82b15f931c193799d75ca31a8c0e69d5154ebe
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-13 14:54:03 +0200

    feat(DataInitializer): for data seeding

---
+++ src/main/kotlin/happybeans/config/DataInitializer.kt
@@ -0,0 +1,10 @@
+package happybeans.config
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class DataInitializer() : CommandLineRunner {
+    override fun run(vararg args: String?) {
+    }
+}
