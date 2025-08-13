225abbb3357d7bdaf52c400c43b7876de633a6e4
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-13 17:19:12 +0200

    feat(UserCreateRequestDto): add check for string with spaces

--- src/main/kotlin/happybeans/dto/user/UserCreateRequestDto.kt
+++ src/main/kotlin/happybeans/dto/user/UserCreateRequestDto.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.Length
 
 class UserCreateRequestDto(
-    @field:NotBlank(message = "Emailcannot be blank")
+    @field:NotBlank(message = "Email cannot be blank")
     @field:Email(message = "Must be a valid email address")
     var email: String,
     @field:Length(min = 6, message = "Password must be at least 6 characters")
@@ -13,4 +13,8 @@ class UserCreateRequestDto(
     @field:Length(min = 3, message = "FirstName must be at least 3 characters")
     var firstName: String,
     var lastName: String,
-)
+) {
+    init {
+        require(email.isNotBlank()) { "Email cannot be blank" }
+    }
+}
