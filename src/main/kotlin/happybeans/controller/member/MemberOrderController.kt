a65be0cca9e0590f531334ec1f6bdf21e06ffe94
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-14 16:50:09 +0200

    feat(MemberOrderController)

--- src/main/kotlin/happybeans/controller/member/MemberOrderController.kt
+++ src/main/kotlin/happybeans/controller/member/MemberOrderController.kt
@@ -1,9 +1,33 @@
 package happybeans.controller.member
 
+import happybeans.dto.order.OrderListResponse
+import happybeans.dto.order.OrderResponse
+import happybeans.model.User
+import happybeans.service.MemberOrderService
+import happybeans.utils.annotations.LoginMember
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/member/orders")
-class MemberOrderController {
+class MemberOrderController(
+    private val orderService: MemberOrderService,
+) {
+    @GetMapping("")
+    fun getAllOrders(
+        @LoginMember user: User,
+    ): ResponseEntity<OrderListResponse> {
+        val response = OrderListResponse(orderService.getAllUserOrders(user.id))
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/{orderId}")
+    fun getOrderById(
+        @PathVariable orderId: Long,
+    ): ResponseEntity<OrderResponse> {
+        return ResponseEntity.ok(orderService.getOrderById(orderId))
+    }
 }
