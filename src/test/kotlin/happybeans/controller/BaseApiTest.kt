600393214b6f0c980fb766a9ab00642f7bae1878
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-15 17:24:38 +0200

    feat: build fix

--- src/test/kotlin/happybeans/controller/BaseApiTest.kt
+++ src/test/kotlin/happybeans/controller/BaseApiTest.kt
@@ -9,6 +9,6 @@ abstract class BaseApiTest {
     @BeforeAll
     fun setup() {
         RestAssured.baseURI = "http://localhost"
-        RestAssured.port = 8080
+        RestAssured.port = 80
     }
 }
