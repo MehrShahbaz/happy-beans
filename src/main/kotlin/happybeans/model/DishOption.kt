f1a20f4842cf4e83a33da8c251f08a4871cc4e67
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-15 11:57:19 +0200

    feat: implement dish and options

---
+++ src/main/kotlin/happybeans/model/DishOption.kt
@@ -0,0 +1,37 @@
+package happybeans.model
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+
+class DishOption(
+    @Column(name = "name", nullable = false)
+    var name: String,
+    @Column(name = "description", nullable = false)
+    var description: String,
+    @Column(name = "price", nullable = false)
+    var price: Double,
+    @Column(name = "image", nullable = false)
+    var image: String,
+    @Column(name = "available", nullable = false)
+    var available: Boolean = true,
+    @OneToOne(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_containers_id")
+    val ingredients: TagContainer,
+    @CreationTimestamp
+    var createdAt: LocalDateTime? = null,
+    @UpdateTimestamp
+    var updatedAt: LocalDateTime? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0L,
+    // TODO handle review
+)
---
+++ src/test/kotlin/happybeans/service/DishServiceTest.kt
@@ -0,0 +1,6 @@
+package happybeans.service
+
+
+class DishServiceTest {
+
+}
