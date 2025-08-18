dd956f7c41fb1bfa3ac1cea2351f35092e745740
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-18 15:39:09 +0200

    refactor: Add stripe dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.stripe:stripe-java:29.4.0")
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
