5bcb2a548835a4cc55d329c29758650436a56c1c
Jin Park <jieun.park.e@deliveryhero.com>
2025-08-21 22:23:14 +0200

    remove: Uncomment parts to remove

--- src/main/kotlin/happybeans/repository/TagContainerRepository.kt
+++ src/main/kotlin/happybeans/repository/TagContainerRepository.kt
@@ -5,13 +5,13 @@ import happybeans.model.TagContainer
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TagContainerRepository : JpaRepository<TagContainer, Long> {
-    fun findByUserIdAndType(
-        userId: Long,
-        type: TagContainerType,
-    ): TagContainer?
-
-    fun findByDishIdAndType(
-        dishId: Long,
-        type: TagContainerType,
-    ): TagContainer?
+//    fun findByUserIdAndType(
+//        userId: Long,
+//        type: TagContainerType,
+//    ): TagContainer?
+//
+//    fun findByDishIdAndType(
+//        dishId: Long,
+//        type: TagContainerType,
+//    ): TagContainer?
 }
