1a9de22d790aa989cd7e44841232890281f73945
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-21 21:19:03 +0200

    feat: Add Health check URI

--- src/main/kotlin/happybeans/controller/health/HealthCheckController.kt
+++ src/main/kotlin/happybeans/controller/health/HealthCheckController.kt
@@ -1,11 +1,13 @@
 package happybeans.controller.health
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/health")
 class HealthCheckController {
+    @GetMapping
     fun healthCheck(): String {
         return "OK"
     }
