40a76d29804d385860481922cd69ed9b70d12bda
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-25 13:00:17 +0200

    test: fix build

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo ">>> [BeforeInstall] Stopping current application if running..."
+set -e
+
+echo ">>> [BeforeInstall] Stopping current Spring Boot application if running..."
 pkill -f 'java -jar' || true
+
+echo ">>> [BeforeInstall] Stopping Docker container if running..."
+if [ $(docker ps -q -f name=happy-beans) ]; then
+    docker stop happy-beans
+    docker rm happy-beans
+fi
 
 echo ">>> [BeforeInstall] Fixing ownership..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/app
 
 echo ">>> [BeforeInstall] Cleaning old files..."
 rm -rf /home/ubuntu/app/*
-
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,15 +6,13 @@ if [ -z "$JAR_FILE" ]; then
   exit 1
 fi
 
-if [ -f "/home/ubuntu/config/.env" ]; then
-    cp /home/ubuntu/config/.env /home/ubuntu/app/.env
-    echo ">>> [ApplicationStart] Copied .env file to application directory."
-else
-    echo ">>> [ApplicationStart] .env file not found at /home/ubuntu/config/.env"
-    exit 1
-fi
-
 echo ">>> ####### Test #######"
-echo ">>> [ApplicationStart] Starting application: $JAR_FILE"
-sudo java -jar $JAR_FILE > /home/ubuntu/app/app.log 2>&1 &
+echo ">>> changing directory"
+cd app
+
+echo ">>> Building image"
+sudo docker build -t happy-beans-image:1.0 .
+
+echo ">>> Running docker image"
+sudo docker run --env-file /home/ubuntu/config/.env -p 80:80 happy-beans-image:1.0
 
