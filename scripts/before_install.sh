fc78b4bfc9f83c350407fc38b6b6356228fe5aec
apoorva-s-natesh <apoorva1326@gmail.com>
2025-08-25 18:41:10 +0200

    Style: Remove docker code

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo ">>> [BeforeInstall] Stopping Docker container if running..."
-CONTAINER_ID=$(docker ps -q -f name=happy-beans)
-if [ -n "$CONTAINER_ID" ]; then
-    docker stop happy-beans
-    docker rm happy-beans
-fi
+#CONTAINER_ID=$(docker ps -q -f name=happy-beans)
+#if [ -n "$CONTAINER_ID" ]; then
+#    docker stop happy-beans
+#    docker rm happy-beans
+#fi
 
 echo ">>> [BeforeInstall] Cleaning old build artifacts..."
 rm -rf /home/ubuntu/app/build/libs/*
