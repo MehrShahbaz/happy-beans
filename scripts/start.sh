1d3c1846e7358dd4e7bf0808a931cabd6c61568c
apoorva-s-natesh <apoorva1326@gmail.com>
2025-08-25 11:43:49 +0200

    build: manage .env on start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,14 @@ if [ -z "$JAR_FILE" ]; then
   exit 1
 fi
 
+if [ -f "/home/ubuntu/config/.env" ]; then
+    cp /home/ubuntu/config/.env /home/ubuntu/app/.env
+    echo ">>> [ApplicationStart] Copied .env file to application directory."
+else
+    echo ">>> [ApplicationStart] .env file not found at /home/ubuntu/config/.env"
+    exit 1
+fi
+
 echo ">>> ####### Test #######"
 echo ">>> [ApplicationStart] Starting application: $JAR_FILE"
 sudo java -jar $JAR_FILE > /home/ubuntu/app/app.log 2>&1 &
