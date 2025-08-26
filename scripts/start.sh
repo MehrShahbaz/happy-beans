a636fba946a27c59d9952c2f47abe07b66dbba8f
apoorva-s-natesh <apoorva1326@gmail.com>
2025-08-27 00:17:55 +0200

    build: Add application properties for different profiles dev,prod and test Tests run on test profile with H2.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-JAR_FILE=$(ls /home/ubuntu/app/build/libs/*.jar | head -n 1)
+JAR_FILE=$(ls /home/ubuntu/app/build/libs/*.jar | head -n 1 || true)
 
-## Load environment variables from .env
-#if [ -f /home/ubuntu/app/.env ]; then
-#  export $(grep -v '^#' /home/ubuntu/app/.env | xargs)
-#fi
-
+# Load environment variables from .env
 if [ -f /home/ubuntu/app/.env ]; then
-  while read line; do
-    export "$line"
-  done < /home/ubuntu/app/.env
+  export $(grep -v '^#' /home/ubuntu/app/.env | xargs)
 fi
 
 if [ -z "$JAR_FILE" ]; then
@@ -19,5 +13,5 @@ fi
 
 echo ">>> ####### Test #######"
 echo ">>> [ApplicationStart] Starting application: $JAR_FILE"
-export $(grep -v '^#' ./.env | xargs)
-sudo -E java -jar $JAR_FILE > /home/ubuntu/app/app.log 2>&1 &
+#nohup java -jar "$JAR_FILE" > /home/ubuntu/app/logs/app.log 2>&1 &
+nohup java -jar "$JAR_FILE" --spring.profiles.active=prod > /home/ubuntu/app/logs/app.log 2>&1 &
