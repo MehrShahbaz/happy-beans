1a0548d5583e34048999c21164a73375bb5de594
apoorva-s-natesh <apoorva1326@gmail.com>
2025-08-26 21:06:09 +0200

    refactor: Add env variable to .env in buildspec.yml

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-JAR_FILE=$(ls /home/ubuntu/app/build/libs/*.jar | head -n 1)
+#find JAR
+JAR_FILE=$(ls /home/ubuntu/app/build/libs/*.jar | head -n 1 || true)
 
-## Load environment variables from .env
-#if [ -f /home/ubuntu/app/.env ]; then
-#  export $(grep -v '^#' /home/ubuntu/app/.env | xargs)
-#fi
-
+# Load environment variables from .env
 if [ -f /home/ubuntu/app/.env ]; then
-  while read line; do
-    export "$line"
-  done < /home/ubuntu/app/.env
+  export $(grep -v '^#' /home/ubuntu/app/.env | xargs)
 fi
 
 if [ -z "$JAR_FILE" ]; then
@@ -17,7 +12,6 @@ if [ -z "$JAR_FILE" ]; then
   exit 1
 fi
 
-echo ">>> ####### Test #######"
 echo ">>> [ApplicationStart] Starting application: $JAR_FILE"
-export $(grep -v '^#' ./.env | xargs)
-sudo -E java -jar $JAR_FILE > /home/ubuntu/app/app.log 2>&1 &
+#export $(grep -v '^#' ./.env | xargs)
+nohup java -jar $JAR_FILE > /home/ubuntu/app/app.log 2>&1 &
