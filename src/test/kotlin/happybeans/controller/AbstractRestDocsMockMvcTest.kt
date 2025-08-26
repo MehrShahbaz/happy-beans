40d17e7ec04c8338db75871b2833061ad362277a
apoorva-s-natesh <apoorva1326@gmail.com>
2025-08-26 21:28:07 +0200

    refactor: remove @ActiveProfiles("test") from @ActiveProfiles("test") Testing if @ActiveProfiles("test") is failing the tests on prod

--- src/test/kotlin/happybeans/controller/AbstractRestDocsMockMvcTest.kt
+++ src/test/kotlin/happybeans/controller/AbstractRestDocsMockMvcTest.kt
@@ -10,11 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 
-@ActiveProfiles("test")
+// @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
