409191b0d99b581e7997c1ff393408a78b427d9f
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-27 16:06:08 +0200

    fix: deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,3 +4,6 @@ sudo pkill -f 'java -jar' || true
 
 echo ">>> [BeforeInstall] Fixing ownership..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/app
+
+echo ">>> Removing old files"
+rm -rf /home/ubuntu/app/*
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 JAR_FILE=$(ls /home/ubuntu/app/build/libs/*.jar | head -n 1 || true)
 
-echo "Please" >> /home/ubuntu/app/.env
+echo "Please" >> /home/ubuntu/app/.env.properties
 
 # Load environment variables from .env
-export $(grep -v '^#' /home/ubuntu/app/.env | xargs)
+export $(grep -v '^#' /home/ubuntu/app/.env.properties | xargs)
 
 if [ -z "$JAR_FILE" ]; then
   echo ">>> [ApplicationStart] No JAR file found!"
