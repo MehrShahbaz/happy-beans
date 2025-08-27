c902d417bbc40d1542ab83997231478413bc25aa
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-27 11:50:39 +0200

    fix: dialect problem

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ">>> [BeforeInstall] Stopping current application if running..."
-pkill -f 'java -jar' || true
+sudo pkill -f 'java -jar' || true
 
 echo ">>> [BeforeInstall] Fixing ownership..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/app
