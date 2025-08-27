0b0a057604011343e4a03fe709e031fd9e99eb82
codebrew42 <ahnwia@gmail.com>
2025-08-27 19:43:24 +0200

    style: Add logger and message for API consumers (frontend/client)

--- src/main/kotlin/happybeans/controller/restaurantOwner/RestaurantOwnerAuthController.kt
+++ src/main/kotlin/happybeans/controller/restaurantOwner/RestaurantOwnerAuthController.kt
@@ -4,6 +4,7 @@ import happybeans.dto.auth.LoginRequestDto
 import happybeans.dto.response.TokenResponse
 import happybeans.service.RestaurantOwnerAuthService
 import jakarta.validation.Valid
+import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController
 class RestaurantOwnerAuthController(
     private val restaurantOwnerAuthService: RestaurantOwnerAuthService,
 ) {
+    private val logger = KotlinLogging.logger {}
+    
     @PostMapping("/login")
     fun login(
         @Valid @RequestBody loginRequestDto: LoginRequestDto,
     ): ResponseEntity<TokenResponse> {
+        logger.info("POST Restaurant Owner login for ${loginRequestDto.email}")
         val token = restaurantOwnerAuthService.login(loginRequestDto)
         return ResponseEntity.ok().body(TokenResponse(token))
     }
