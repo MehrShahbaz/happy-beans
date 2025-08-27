88dd67ca05aa142a1646b5f8598129a463fa720e
codebrew42 <ahnwia@gmail.com>
2025-08-27 19:08:10 +0200

    fix: Use a much simpler TagRequest DTO.

---
+++ src/main/kotlin/happybeans/dto/tag/TagRequest.kt
@@ -0,0 +1,8 @@
+package happybeans.dto.tag
+
+import jakarta.validation.constraints.NotEmpty
+
+data class TagRequest(
+    @field:NotEmpty(message = "Tag names cannot be empty")
+    val tagNames: List<String>
+)
