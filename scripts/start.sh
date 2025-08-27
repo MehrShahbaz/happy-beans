0cbbaa4834152581be2b3e093e8e55a6422cc135
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-27 18:01:55 +0200

    fix: deployment

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 JAR_FILE=$(ls /home/ubuntu/app/build/libs/*.jar | head -n 1 || true)
 if [ -z "$JAR_FILE" ]; then
@@ -18,7 +19,10 @@ if ! command -v authbind >/dev/null 2>&1; then
   exit 1
 fi
 
+echo "Changing directory..."
 cd /home/ubuntu/app
+echo "Directory changed to $(pwd)"
+
 echo ">>> $(date '+%Y-%m-%d %H:%M:%S') [ApplicationStart] Starting application: $JAR_FILE"
 
 nohup authbind --deep java -jar /home/ubuntu/app/build/libs/happy-beans-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /home/ubuntu/app/logs/app.log 2>&1 &
