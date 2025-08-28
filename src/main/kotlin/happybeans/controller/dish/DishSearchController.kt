edd3670e9d40b70a87c1743d85e9863254ed64bf
apoorva-s-natesh <apoorva1326@gmail.com>
2025-08-28 20:45:48 +0200

    feat: Merge Main to develop

--- src/main/kotlin/happybeans/controller/dish/DishSearchController.kt
+++ src/main/kotlin/happybeans/controller/dish/DishSearchController.kt
@@ -3,6 +3,7 @@ package happybeans.controller.dish
 import happybeans.model.DishOption
 import happybeans.model.User
 import happybeans.service.DishService
+import happybeans.service.UserService
 import happybeans.utils.annotations.LoginMember
 import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/member/dishes/search")
 class DishSearchController(
     private val dishService: DishService,
+    private val userService: UserService,
 ) {
     private val logger = KotlinLogging.logger {}
 
@@ -22,7 +24,10 @@ class DishSearchController(
         @LoginMember user: User,
     ): ResponseEntity<List<DishOption>> {
         logger.info("GET Dishes by user tags for user: ${user.id}")
-        val filteredDishOptions = dishService.getFilteredDishOptionsByUser(user)
+        val likes = userService.getUserLikes(user.id).map { it.name }.toSet()
+        val dislikes = userService.getUserDislikes(user.id).map { it.name }.toSet()
+
+        val filteredDishOptions = dishService.getFilteredDishOptionsByUserTags(likes, dislikes)
         return ResponseEntity.ok(filteredDishOptions)
     }
 }
