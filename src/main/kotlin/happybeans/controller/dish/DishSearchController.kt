25034ef6f8be879f98d6a8a7122e86865a3ae5e3
codebrew42 <jieun.park.e@deliveryhero.com>
2025-08-28 17:32:18 +0200

    chore: Add transactional comments for later use

--- src/main/kotlin/happybeans/controller/dish/DishSearchController.kt
+++ src/main/kotlin/happybeans/controller/dish/DishSearchController.kt
@@ -6,6 +6,7 @@ import happybeans.service.DishService
 import happybeans.utils.annotations.LoginMember
 import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
+//import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,6 +19,7 @@ class DishSearchController(
     private val logger = KotlinLogging.logger {}
 
     @GetMapping()
+//    @Transactional(readOnly = true)
     fun getFilteredDishOptionsByUser(
         @LoginMember user: User,
     ): ResponseEntity<List<DishOption>> {
